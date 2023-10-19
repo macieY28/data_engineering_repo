731ed7b0d58dc9027ee5bb9b0045e94bfa929de6
macieY28 <143354365+macieY28@users.noreply.github.com>
2023-10-19 12:08:26 +0200

    Delete sql/06_credentials_pyspark.sql

--- sql/06_credentials_pyspark.sql
+++
@@ -1,8 +0,0 @@
-USE master
-CREATE LOGIN sparkLogin WITH PASSWORD = 'pyPass236@';
-
-USE EcommerceFootballStore
-CREATE USER sparkUser FOR LOGIN sparkLogin
-
-ALTER ROLE db_datareader ADD MEMBER sparkUser
-ALTER ROLE db_datawriter ADD MEMBER sparkUser
